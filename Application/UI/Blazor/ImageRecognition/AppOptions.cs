924a52c656dba11282985b48b3af145df4f0903e
sagulati <sagulati@amazon.com>
2021-06-11 16:05:58 -0600

    Adding basic Blazor Server with Authentication Functionality.

---
+++ Application/UI/Blazor/ImageRecognition/AppOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageRecognition.BlazorFrontend.Frontend
+{
+    public class AppOptions
+    {
+        public string ImageRecognitionApiUrl { get; set; }
+
+        public string WebSocketApi { get; set; }
+
+        public string PhotoStorageBucket { get; set; }
+
+        public string UploadBucketPrefix { get; set; }
+    }
+}
